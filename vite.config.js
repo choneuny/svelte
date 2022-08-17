a6dc0b40fb074120e7ce84da507137ff849f4e69
IAspede <choneunyw1@gmail.com>
2022-08-17 15:06:12 +0900

    WIP_0817

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "",
+	build: {
+		rollupOptions: {
+			input: {
+				main: "./index.html",
+			},
+		},
+	},
 	plugins: [
 		svelte({
 			preprocess: sveltePreprocess(),
